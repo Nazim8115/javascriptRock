8e5e37770b8a46ee6978642c487eecef5fcd0e81
MOH NAZIM <82774815+Nazim8115@users.noreply.github.com>
2023-11-28 12:07:21 +0000

    basic random code

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,37 @@
-console.log("hello this is js program ")
+console.log("hello this is js program ");
+// var x = 700;
+// console.log(x) 
+// var  a = "nazim"
+// var a = "nazim khan"
+// console.log(a)
+
+// settime out function 
+// setTimeout(() => {
+//     let a = "hello this is the  setTime out function ";
+//     console.log(a);
+// }, 5000);
+
+
+// setInterval(() => {
+//     console.log("hi rocky!")
+// }, 2000);
+
+
+
+// for loop 
+
+const arr  = [1,2,3,4,5,6,7,8,"end"];
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    console.log(element)
+    
+}
+// check type of array by default array is an object 
+
+console.log(typeof(arr)) //return object ok..
+console.log(Array.isArray(arr))// return true it's mean its is an arry 
+
+
+
+
