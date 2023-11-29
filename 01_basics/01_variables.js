6cecb512cc488d8b8ce146145c175afa51d4844d
MOH NAZIM <82774815+Nazim8115@users.noreply.github.com>
2023-11-29 17:55:25 +0000

    learn about variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 78612
+let accountEmail= "nazim.nn459@gmail.com"
+var accountPassword = "12345"
+accountCity = "Noida"
+console.log(accountId,accountEmail,accountPassword,accountCity);
+// accountId = 887227 // we can't this because constant never will change  
+accountEmail = "mnk123@gmail.com"
+accountPassword = "82727";
+accountCity= "delhi"
+console.log(accountEmail,accountPassword,accountCity);
